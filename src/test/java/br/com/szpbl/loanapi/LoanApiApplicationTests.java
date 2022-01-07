fb4a4cc3c3c12e301e2f2725982281bc3098acb1
Pablo Souza <pablo.osouza@hotmail.com>
2022-01-07 17:17:06 -0300

    create project

---
+++ src/test/java/br/com/szpbl/loanapi/LoanApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.szpbl.loanapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoanApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
