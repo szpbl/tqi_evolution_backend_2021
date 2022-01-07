a57f37b1081bc21d35bf27c467bce3de577b1504
Pablo Souza <pablo.osouza@hotmail.com>
2022-01-07 17:32:26 -0300

    create customer repository

---
+++ src/main/java/br/com/szpbl/loanapi/repositories/CustomerRepository.java
@@ -0,0 +1,10 @@
+package br.com.szpbl.loanapi.repositories;
+
+import br.com.szpbl.loanapi.entities.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface CustomerRepository extends JpaRepository <Customer, Long> {
+    Optional<Customer> findByEmail(String email);
+}
